9cc48d8269033c4173fab10af868e7f2a919d260
Olazee04 <owoiluzainaboluwatoyin@gmail.com>
2025-06-21 00:43:30 +0100

    Initial commit - VehicleApp

---
+++ VehicleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace VehicleApp
+{
+    class Vehicle
+    {
+        public string Make { get; set; }
+        public string Colour { get; set; }
+        public int Year { get; set; }
+
+        public Vehicle(string make, string colour, int year)
+        {
+            Make = make;
+            Colour = colour;
+            Year = year;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Vehicle Information:");
+            Console.WriteLine($"Make: {Make}");
+            Console.WriteLine($"Colour: {Colour}");
+            Console.WriteLine($"Year: {Year}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle myCar = new Vehicle("Toyota Yaris", "Red", 2020);
+            myCar.DisplayInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
